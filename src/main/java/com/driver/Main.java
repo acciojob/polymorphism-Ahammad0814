8981940bda502475d94c89847374b39a8ac62342
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 20:48:31 +0530

    Update Main.java\

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-
+  public static void main(String[] args){
+    
+  }
+  static int product(int z, int y){
+    
+  }
+  static int product(int x, int z, int y){
+    
+  }
+  static double product(double z, double y){
+    
+  }
 }
