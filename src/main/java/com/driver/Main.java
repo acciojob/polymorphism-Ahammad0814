86ea6f93ac13802c8f0dec9009a017fd0916d4ca
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 20:51:38 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,15 +2,17 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-    
+    product(10, 20);
+    product(10, 20, 30);
+    product(10.00000, 20.00000);
   }
   static int product(int z, int y){
-    
+    return z * y;
   }
   static int product(int x, int z, int y){
-    
+    return x * z * y;
   }
   static double product(double z, double y){
-    
+    return (double) (z * y);
   }
 }
